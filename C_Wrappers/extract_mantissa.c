dae003dc6c452750130387373d7ae74216f54e24
Saswat Levin <saswatlevin@gmail.com>
2020-02-12 22:37:02 +0530

    Added extract_mantissa.c with timing and print statements

--- C_Wrappers/extract_mantissa.c
+++ C_Wrappers/extract_mantissa.c
@@ -15,64 +15,60 @@ void print_int_bits(int num)
    }
 }
 
-// bit returned at location
-/*static inline int bit_return(int a, int loc)   
-{
-    int buf = a & 1<<loc;
-
-    if (buf == 0) return 0;
-    else return 1; 
-}*/
-
 //Clear a bit at a given location and then set the bit if specified 
-static inline void transfer_bits_to_int(float number)
+static inline uint8_t transfer_bits_to_int(float number,int number_of_bits)
 {
 	int i=31;
 	int *b;
 	b=&number;
-	int temp=0;
-	uint8_t tmp_8=0;
+	int bit_store_32=0;
+	uint8_t bit_store_8=0;
 	printf("\nBefore setting bits=");
-	print_int_bits(temp);
+	print_int_bits(bit_store_32);
 
-	for (i=31;i>=0;i--)
+	for (i=number_of_bits;i>=0;i--)
 	{
 		if (BIT_RETURN(*b,i)==1)
 		{
 			//temp = (temp & (~(1 << temp))) | (1 << i);
-			temp |= 1 << i;
+			bit_store_32 |= 1 << i;
 		}
 	}
 	printf("\nAfter setting bits=");
-	print_int_bits(temp);
-	printf("\ntemp=%d\t",temp);
-	tmp_8=(uint8_t)temp;
-	printf("\ntmp_8=%d\t",tmp_8);
-	
+	print_int_bits(bit_store_32);
+	bit_store_8=(uint8_t)bit_store_32;
+	return bit_store_8;
 }
 
+//Print bits of a float
+static inline void print_float_bits(float number)
+{
+  int *float_ptr;
+  float_ptr=&number;
+  printf("\nBits of float a=");
+  int i=0;
+    for (i = 31; i>=0; i--)
+    {
+        printf("%d",BIT_RETURN(*float_ptr,i));
+    }
+}
 
-int main(int argc, char **argv[])
+void main(int argc, char **argv[])
 {
 	
 	 //11000010111011010100000000000000  
     // 1 sign bit | 8 exponent bit | 23 fraction bits
     //float a = -118.625; 
-    float a=0.129195;
-    int *b;
-    b = &a;
+    float a=0.43072589;
+    uint8_t bit_store_8=0;
     printf("\na=%f\t",a);
-    int i;
-    printf("\nBits of float a=");
-    for (i = 31; i>=0; i--)
-    {
-        printf("%d",BIT_RETURN(*b,i));
-    }
-    //clock_t begin=clock();
-    transfer_bits_to_int(a);
-    //clock_t end=clock();
-    //double time_spent=(double)(end-begin)/CLOCKS_PER_SEC;
-    //printf("\ntime_spent=%f\t",time_spent);
-    return 0;
+    print_float_bits(a);
+    clock_t begin=clock();
+    bit_store_8= transfer_bits_to_int(a,1);
+    clock_t end=clock();
+    double time_spent=(double)(end-begin)/CLOCKS_PER_SEC;
+    printf("\ntime_spent=%f\t",time_spent);
+    printf("\nbit_store_8=%d\t",bit_store_8);
+    //return 0;
 }
 
